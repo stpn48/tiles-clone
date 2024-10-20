2185a5e14e8bd148a499b149178d5cdefb54f4af
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-20 20:27:39 +0200

    Update GameGrid.tsx

--- src/app/game/components/GameGrid.tsx
+++ src/app/game/components/GameGrid.tsx
@@ -1,15 +1,43 @@
-import tiles from "@/assets/testTileSet.json";
-import React from "react";
-import { GridRow } from "./GridRow";
+"use client";
 
-type Props = {};
+import { useShowFinishedModal } from "@/hooks/useShowFinishedModal";
+import { useGameLogicStore } from "@/store/gameLogicStore";
+import { generateGrid } from "@/utils/generateGrid";
+import { AnimatePresence } from "framer-motion";
+import React, { useEffect, useState } from "react";
+import { Cell } from "./Cell";
+import { FinishModal } from "./FinishModal";
+import { GameInstructionsModal } from "./GameInstructionsModal";
+import { QuestionMarkCircleButton } from "./QuestionMarkCircleButton";
 
-export function GameGrid({}: Props) {
+export function GameGrid() {
+  const { grid, setGrid, gameFinished } = useGameLogicStore();
+
+  const { showFinishedModal } = useShowFinishedModal(gameFinished);
+
+  const [showGameInstructionsModal, setShowGameInstructionsModal] = useState(true);
+
+  useEffect(() => {
+    const grid = generateGrid();
+    setGrid(grid);
+  }, [setGrid]);
+
   return (
-    <div className="flex flex-col">
-      {tiles.map((row, rowIndex) => (
-        <GridRow key={rowIndex} row={row} />
-      ))}
+    <div className="flex">
+      <div className="w grid h-[486px] w-[405px] grid-cols-5 grid-rows-6">
+        {grid.map((cell, cellIndex) => (
+          <Cell cellIndex={cellIndex} key={cellIndex} cell={cell} />
+        ))}
+        <QuestionMarkCircleButton setShowGameInstructionsModal={setShowGameInstructionsModal} />
+
+        {showFinishedModal && <FinishModal closeModal={() => {}} />}
+
+        <AnimatePresence>
+          {showGameInstructionsModal && (
+            <GameInstructionsModal closeModal={() => setShowGameInstructionsModal(false)} />
+          )}
+        </AnimatePresence>
+      </div>
     </div>
   );
 }
