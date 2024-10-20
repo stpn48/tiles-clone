2dd2bc7b87a5e133fdf531d62d016b3861587a26
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-20 20:27:49 +0200

    Update gameLogicStore.ts

--- src/store/gameLogicStore.ts
+++ src/store/gameLogicStore.ts
@@ -1,10 +1,44 @@
+import { Pattern } from "@/types/types";
+import { stat } from "fs";
 import { create } from "zustand";
 
 interface Store {
-  firstTilePatternIds: string[] | null;
-  setFirstTilePatternIds: (patternIds: string[]) => void;
+  grid: Pattern[][];
+  setGrid: (grid: Pattern[][] | ((prev: Pattern[][]) => Pattern[][])) => void;
+
+  firstCellIndex: number | null;
+  setFirstCellIndex: (index: number | null) => void;
+
+  combo: number;
+  setCombo: (combo: number | ((prev: number) => number)) => void;
+
+  maxCombo: number;
+  setMaxCombo: (maxCombo: number) => void;
+
+  perfectCombo: boolean;
+  setPerfectCombo: (perfectCombo: boolean) => void;
+
+  gameFinished: boolean;
+  setGameFinished: (finished: boolean) => void;
 }
 export const useGameLogicStore = create<Store>((set) => ({
-  firstTilePatternIds: null,
-  setFirstTilePatternIds: (id) => set({ firstTilePatternIds: id }),
+  grid: [],
+  setGrid: (grid) =>
+    set((state) => ({ grid: typeof grid === "function" ? grid(state.grid) : grid })),
+
+  firstCellIndex: null,
+  setFirstCellIndex: (index) => set({ firstCellIndex: index }),
+
+  combo: 0,
+  setCombo: (combo) =>
+    set((state) => ({ combo: typeof combo === "function" ? combo(state.combo) : combo })),
+
+  maxCombo: 0,
+  setMaxCombo: (maxCombo) => set({ maxCombo }),
+
+  perfectCombo: true,
+  setPerfectCombo: (perfectCombo) => set({ perfectCombo }),
+
+  gameFinished: false,
+  setGameFinished: (finished) => set({ gameFinished: finished }),
 }));
